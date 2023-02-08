611de5af4e2fadbe8dbd5295f6f05125e18ab9d5
Tim Becker <tim.becker@mni.thm.de>
2023-02-08 10:49:56 +0100

    import fonts + set up static assets

--- mvc-todo-vue/src/App.vue
+++ mvc-todo-vue/src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue';
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <h1>ToDo-App</h1>
-    <div class="wrapper">
-      <HelloWorld  />
+    <div class="row container heading">
+      <h1>ToDo-App</h1>
+      <img src="./assets/icons/icon-sun.svg" alt="">
+      <!-- <img src="../assets/icons/icon-sun.svg" alt=""> -->
+      <!-- <img src="../public/assets/icons/icon-moon.svg" alt=""> -->
     </div>
+
   </header>
 
   <main>
+    <ToDoList />
   </main>
 </template>
 
 <style scoped>
-
+.heading{
+  justify-content: space-between;
+}
 </style>
---
+++ mvc-todo-vue/src/components/ToDoList.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+    // props: {
+    // },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="todoItem row">
+        <div class="circle"></div>
+        <input type="text" placeholder="type ToDo here..">
+    </div>
+</template>
+
+<style scoped>
+.circle{
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    border: 1px solid var(--White-opac05);
+}
+</style>
