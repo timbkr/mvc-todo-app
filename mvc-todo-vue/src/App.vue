13c2f187e4bcf75d05e8ca854a8b54c73b73fc67
Tim Becker <tim.becker@mni.thm.de>
2023-02-08 20:52:17 +0100

    first working draft

--- mvc-todo-vue/src/App.vue
+++ mvc-todo-vue/src/App.vue
@@ -3,24 +3,27 @@ import ToDoList from './components/ToDoList.vue';
 </script>
 
 <template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     <div class="row container heading">
       <h1>ToDo-App</h1>
       <img src="./assets/icons/icon-sun.svg" alt="">
-      <!-- <img src="../assets/icons/icon-sun.svg" alt=""> -->
-      <!-- <img src="../public/assets/icons/icon-moon.svg" alt=""> -->
+      <!-- <img src="./assets/icons/icon-sun.svg" alt=""> -->
     </div>
 
   </header>
 
   <main>
-    <ToDoList />
+    <div class="container">
+      <ToDoList />
+
+    </div>
   </main>
 </template>
 
 <style scoped>
-.heading{
+.heading {
   justify-content: space-between;
+  padding: 2em 0;
 }
 </style>
--- mvc-todo-vue/src/components/ToDoList.vue
+++ mvc-todo-vue/src/components/ToDoList.vue
@@ -4,25 +4,190 @@ export default {
     // },
     data() {
         return {
-
+            todos: [
+                {
+                    id: 0,
+                    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, suscipit. Doloremque soluta distinctio explicabo?",
+                    checked: true
+                },
+                {
+                    id: 1,
+                    text: "Test",
+                    checked: false
+                }
+            ],
+            inputText: "",
+            inputChecked: false,
+            filterActive: false,
+            filterCompleted: false,
+        }
+    },
+    methods: {
+        addTodo(text: string, checked: boolean) {
+            let id;
+            if (this.todos.length > 0) {
+                id = this.todos[this.todos.length - 1].id + 1;
+            }
+            else id = 0
+            const item = {
+                id: id,
+                text: text,
+                checked: checked
+            }
+            this.todos.push(item)
+            this.inputText = "";
+        },
+        deleteTodo(id: number) {
+            const elem = this.todos.find(elem => elem.id === id)
+            const index = this.todos.indexOf(elem!);
+            this.todos.splice(index, 1)
+        },
+        checkTodo(id: number) {
+            const elem = this.todos.find(elem => elem.id === id)
+            const index = this.todos.indexOf(elem!);
+            this.todos[index].checked = !this.todos[index].checked;
+        },
+        setFilter(mode: number) {
+            this.filterActive = false;
+            this.filterCompleted = false;
+            if (mode === 0) return // 'all'
+            else if (mode === 1) this.filterActive = true; //'active'
+            else if (mode === 2) this.filterCompleted = true; //'completed' 
+        }
+    },
+    computed: {
+        filterTodos() {
+            if (this.filterActive)
+                return this.todos.filter(elem => elem.checked === false);
+            else if (this.filterCompleted)
+                return this.todos.filter(elem => elem.checked === true);
+            else return this.todos;
         }
     }
 }
 </script>
 
 <template>
+    <div class="todoItem content-section">
+        <div @click="inputChecked = !inputChecked" class="circle" :class="{circleActive: inputChecked}">
+            <img v-show="inputChecked" class="checkImg" src="../assets/icons/icon-check.svg" alt="">
+        </div>
+        <input type="text" v-model="inputText" @keydown.enter="addTodo(inputText, inputChecked)"
+            placeholder="type ToDo here..">
+    </div>
 
-    <div class="todoItem row">
-        <div class="circle"></div>
-        <input type="text" placeholder="type ToDo here..">
+    <div class="todo-list content-section">
+        <div class="todoItem" v-for="(item, index) in filterTodos">
+            <div @click="checkTodo(item.id)" class="circle" :class="{ circleActive: item.checked }">
+                <img v-show="item.checked" class="checkImg" src="../assets/icons/icon-check.svg" alt="">
+            </div>
+            <p @click="checkTodo(item.id)" :class="{ completed: item.checked }">{{ item.text }}</p>
+            <button @click="deleteTodo(item.id)"><img src="../assets/icons/icon-cross.svg" alt="" /></button>
+        </div>
+
+        <div class="todoItem lastTodoRow">
+            <div>{{ filterTodos.length }} items left</div>
+            <button class="clearBTN">Clear Completed</button>
+        </div>
+    </div>
+
+    <div class="todoFilterRow content-section">
+        <button @click="setFilter(0)" :class="{ filterActive: !filterActive && !filterCompleted }">All</button>
+        <button @click="setFilter(1)" :class="{ filterActive: filterActive }">Active</button>
+        <button @click="setFilter(2)" :class="{ filterActive: filterCompleted }">Completed</button>
     </div>
 </template>
 
 <style scoped>
-.circle{
-    width: 50px;
-    height: 50px;
+.content-section {
+    background-color: var(--color-elements);
+    border-radius: .5em;
+}
+
+.todo-list {
+    margin: 1em 0;
+}
+
+.todoItem {
+    display: flex;
+    align-items: center;
+    padding: 0.8em 1.2em;
+    gap: 0.7em;
+    border-top: 1px solid var(--color-text-opac-0-5);
+}
+
+.todoItem:first-child {
+    border: 0;
+}
+
+.todoItem p {
+    flex: 1;
+    cursor: pointer;
+}
+
+.completed {
+    text-decoration: line-through;
+    color: var(--color-text-opac-0-5);
+}
+
+.circle {
+    width: 25px;
+    height: 25px;
+    /* margin: 1em; */
     border-radius: 100%;
-    border: 1px solid var(--White-opac05);
+    border: 1px solid var(--color-text-opac-0-5);
+    display: flex;
+    padding: 0.3em;
+    cursor: pointer;
+}
+
+.circle:hover {
+    /* ToDO: linear gradient border */
+}
+
+.circleActive {
+    background: linear-gradient(135deg, rgba(87, 221, 255, 1) 0%, rgba(192, 88, 243, 1) 100%);
+    border: 0;
+}
+
+.checkImg {
+    width: 100%
+}
+
+input[type=text] {
+    background-color: var(--color-elements);
+    border: 0;
+    flex: 1;
+}
+
+input[type=text]:focus {
+    outline: transparent;
+}
+
+.lastTodoRow {
+    color: var(--color-text-opac-0-5);
+    justify-content: space-between;
+    /* ToDo: min height for this elem */
+}
+
+.todoFilterRow {
+    display: flex;
+    justify-content: center;
+    gap: 1em;
+    padding: 1em;
+}
+
+button {
+    background-color: transparent;
+    color: var(--color-text-opac-0-5);
+    border: 0;
+}
+
+button:hover {
+    color: var(--color-text-hover)
+}
+
+.filterActive {
+    color: var(--color-text-active)
 }
 </style>
