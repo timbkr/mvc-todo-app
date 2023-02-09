ecbc9bb528e6c7e5e9dda7176e14caa1179ff1d8
Tim Becker <tim.becker@mni.thm.de>
2023-02-09 20:01:59 +0100

    change delete-left icon

--- mvc-todo-vue/src/components/ToDoList.vue
+++ mvc-todo-vue/src/components/ToDoList.vue
@@ -101,7 +101,8 @@ export default {
         </div>
         <input type="text" v-model="inputText" @keydown.enter="addTodo(inputText, inputChecked)"
             @keydown.esc="toggleInputChecked" placeholder="Create a new todo..">
-        <img style="width: 30px; cursor: pointer" @click="inputText = ''" @keypress.enter="inputText = ''"  tabindex="0" src="../assets/icons/icon-back.svg" alt="">
+        <!-- <img style="width: 30px; cursor: pointer" @click="inputText = ''" @keypress.enter="inputText = ''"  tabindex="0" src="../assets/icons/icon-back.svg" alt=""> -->
+        <img style="width: 30px; cursor: pointer" @click="inputText = ''" @keypress.enter="inputText = ''"  tabindex="0" src="../assets/icons/delete-left.png" alt="">
     </div>
 
     <div class="todo-list content-section">
