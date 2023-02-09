58e5e2e1ac138af79732d6cacaaad59a8df185f7
Tim Becker <tim.becker@mni.thm.de>
2023-02-09 04:29:21 +0100

    add darkMode Toggle

--- mvc-todo-vue/src/App.vue
+++ mvc-todo-vue/src/App.vue
@@ -2,12 +2,46 @@
 import ToDoList from './components/ToDoList.vue';
 </script>
 
+<script lang="ts">
+export default{
+  data(){
+    return{
+      isDarkModeActive: false
+    }
+  },
+  methods:{
+    toggleDarkMode(){
+      this.isDarkModeActive = !this.isDarkModeActive
+      this.setLocalStorage();
+      if(this.isDarkModeActive)
+        document.body.classList.add('lightMode')
+      else document.body.classList.remove('lightMode')
+    },
+    getLocalStorage(){
+      const darkMode = localStorage.getItem('darkMode');
+      if(darkMode === 'enabled') return true
+      else if(darkMode === 'disabled') return false
+      return this.isDarkModeActive
+    },
+    setLocalStorage(){
+      if(this.isDarkModeActive) localStorage.setItem('darkMode','enabled')
+      else localStorage.setItem('darkMode','disabled')
+    }
+  },
+  mounted(){
+    //set Light / Darkmode to saved mode
+    if(this.getLocalStorage() !== this.isDarkModeActive) this.toggleDarkMode();
+    console.log(this.isDarkModeActive);
+    
+  }
+}
+</script>
 <template>
   <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
   <header>
     <div class="row container heading">
       <h1>ToDo-App</h1>
-      <img src="./assets/icons/icon-sun.svg" alt="">
+      <img src="./assets/icons/icon-sun.svg" alt="" @click="toggleDarkMode">
       <!-- <img src="./assets/icons/icon-sun.svg" alt=""> -->
     </div>
 
